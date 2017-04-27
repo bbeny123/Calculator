a7506787137fa66bc17ea30fb125715bd3e9a38b
Kamil Wasilewski <bbeny123@gmail.com>
2017-04-27 21:42:33 +0200

    add Simple layout

--- src/main/java/com/bbeny/calculator/MainActivity.java
+++ src/main/java/com/bbeny/calculator/MainActivity.java
@@ -1,14 +1,51 @@
 package com.bbeny.calculator;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button simpleButton = (Button) findViewById(R.id.simpleButton);
+        simpleButton.setOnClickListener(this);
+        Button advancedButton = (Button) findViewById(R.id.advancedButton);
+        advancedButton.setOnClickListener(this);
+        Button aboutButton = (Button) findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(this);
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.simpleButton: {
+                Intent intent = new Intent(this, SimpleActivity.class);
+                startActivity(intent);
+                break;
+            }
+            /*
+            case R.id.advancedButtonButton: {
+                Intent intent = new Intent(this, AdvancedActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.aboutButton: {
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
+            }
+            */
+            case R.id.exitButton: {
+                finish();
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/com/bbeny/calculator/SimpleActivity.java
@@ -0,0 +1,13 @@
+package com.bbeny.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+    }
+}
